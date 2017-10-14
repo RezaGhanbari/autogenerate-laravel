2f5e8489a7fc4e634878e561ac47dfc669964d44
Reza Ghanbari <rezapython@gmail.com>
2017-10-14 12:55:32 +0330

    new_changes

--- create_laravel_app.sh
+++ create_laravel_app.sh
@@ -4,6 +4,7 @@ composer install
 yarn install
 touch README.md
 cp .env.example .env
+rm -rf .env.example
 git init
 touch .gitignore
 echo '.idea/' >> .gitignore
@@ -11,6 +12,7 @@ echo '.gitignore' >> .gitignore
 echo 'node_modules' >> .gitignore 
 git add -A
 git commit -m 'Initail commit'
+git checkout -b develop
 php artisan key:generate
 php artisan config:clear
 php artisan config:cache
